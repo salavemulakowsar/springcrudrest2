50e59e49008b52dcb3984e0b51a4b1a73c675058
kowsar <salavemulak@gmail.com>
2021-05-05 16:50:59 +0530

    initial commit

---
+++ crudrest2/src/main/java/com/example/demo/Crudrest2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crudrest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crudrest2Application.class, args);
+	}
+
+}
